b398f12640689788829bd5e9735e6bbd158600c6
Harvey Kelly <h.kelly@validus-ivc.co.uk>
2015-03-26 12:09:56 +0000

    Added logging and argument validation to CanaryService

--- CanaryServer/src/main/java/org/canary/server/service/CanaryService.java
+++ CanaryServer/src/main/java/org/canary/server/service/CanaryService.java
@@ -1,5 +1,7 @@
 package org.canary.server.service;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.canary.server.model.Canary;
 import org.canary.server.repository.CanaryRepository;
 import org.canary.server.repository.CrudRepository;
@@ -9,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public final class CanaryService extends AbstractService<Canary> {
+
+    private static final Logger LOGGER = Logger.getLogger(CanaryService.class);
 
     @Autowired
     private CanaryRepository repository;
@@ -20,11 +24,23 @@ public final class CanaryService extends AbstractService<Canary> {
     @Override
     @Transactional
     public Canary create(final String message) {
+
+	LOGGER.debug("create[message=" + message + "]");
+
+	if (StringUtils.isBlank(message)) {
+
+	    throw new IllegalArgumentException(
+		    "Illegal argument; message cannot be blank.");
+	}
+
 	return this.repository.create(message);
     }
 
     @Override
     public CrudRepository<Canary> getRepository() {
+
+	LOGGER.debug("getRepository");
+
 	return this.repository;
     }
 

1b23ef2fb3269c284fc56dfc2354a605339c7c8e
Harvey Kelly <h.kelly@validus-ivc.co.uk>
2015-03-26 12:05:27 +0000

    Added logging and argument validation to CanaryRepository

--- CanaryServer/src/main/java/org/canary/server/repository/CanaryRepository.java
+++ CanaryServer/src/main/java/org/canary/server/repository/CanaryRepository.java
@@ -1,5 +1,7 @@
 package org.canary.server.repository;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.canary.server.model.Canary;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
@@ -7,12 +9,23 @@ import org.springframework.stereotype.Repository;
 @Repository
 public final class CanaryRepository extends AbstractRepository<Canary> {
 
+    private static final Logger LOGGER = Logger
+	    .getLogger(CanaryRepository.class);
+
     private CanaryRepository() {
 	super();
     }
 
     @Override
     public Canary create(final String message) {
+
+	LOGGER.debug("create[message=" + message + "]");
+
+	if (StringUtils.isBlank(message)) {
+
+	    throw new IllegalArgumentException(
+		    "Illegal argument; message cannot be blank.");
+	}
 
 	final Canary canary = new Canary();
 	final Session session = super.getSession();
@@ -26,6 +39,9 @@ public final class CanaryRepository extends AbstractRepository<Canary> {
 
     @Override
     public Class<Canary> getModelClass() {
+
+	LOGGER.debug("getModelClass");
+
 	return Canary.class;
     }
 

05e03b1d0b8738102531f750dd171bfe56a2c737
Harvey Kelly <h.kelly@validus-ivc.co.uk>
2015-04-13 15:31:57 +0100

    Macker: Added macker configuration and build target
    commit 8c880473469771de23ee79a6d2e94389c780d683
    Author: Harvey Kelly <h.kelly@validus-ivc.co.uk>
    Date:   Mon Apr 13 15:29:08 2015 +0100
    
        Added macker lib to .gitignore
    
    commit c9f834be738f9541498f450b8963db4f6c1ed8bd
    Author: Harvey Kelly <h.kelly@validus-ivc.co.uk>
    Date:   Mon Apr 13 15:28:55 2015 +0100
    
        Added macker config
    
    commit 3b984be6ac349f7894381c2cc4ba36e97f9b3b5f
    Author: Harvey Kelly <h.kelly@validus-ivc.co.uk>
    Date:   Mon Apr 13 15:28:34 2015 +0100
    
        Added macker dependencies to ivy.xml
    
    commit 7654ff70d1f4914861330a81e1b08c5890599480
    Author: Harvey Kelly <h.kelly@validus-ivc.co.uk>
    Date:   Mon Apr 13 15:28:12 2015 +0100
    
        Updated classpaths and fixed typos in build.xml
    
    commit c068ec722f285efb517c6c3664ef3212e0c277f3
    Author: Harvey Kelly <h.kelly@validus-ivc.co.uk>
    Date:   Mon Apr 13 15:27:36 2015 +0100
    
        Updated context configuration in CanaryServiceIntegrationTest
    
    commit 00f505d05c0a2299ae0a499b07ac44cd1ab509ff
    Author: Harvey Kelly <h.kelly@validus-ivc.co.uk>
    Date:   Tue Apr 7 08:58:56 2015 +0100
    
        Added macker to build.xml, config outstanding

--- CanaryServer/src/test/java/org/canary/server/service/CanaryServiceIntegrationTest.java
+++ CanaryServer/src/test/java/org/canary/server/service/CanaryServiceIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/applicationContext.xml")
+@ContextConfiguration("classpath:applicationContext.xml")
 public final class CanaryServiceIntegrationTest {
 
     @Autowired
